1654208636bcad7a6d641daa286cea10f5113e9f
yxiangl43 <yxiangl@outlook.com>
2017-06-16 16:28:58 -0700

    java final pratice

---
+++ C2Pratice.java
@@ -0,0 +1,25 @@
+package finalPratice;
+
+import java.util.Scanner;
+
+public class C2Pratice {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+//		///2.10
+//		double purchase = 19.93;
+//		double payment = 20.00;
+//		float change = (float)(payment - purchase);
+//		System.out.println(change);
+	
+		//2.13
+//		String word = in.nextLine();
+//		System.out.println(word.charAt(0));
+//		System.out.println(word.charAt(word.length()-1));
+//		System.out.println(word.substring(1, word.length()-1));
+		
+		System.out.println(3*1000*1000*1000);
+		System.out.println(3.0*1000*1000*1000);
+	}
+
+}
